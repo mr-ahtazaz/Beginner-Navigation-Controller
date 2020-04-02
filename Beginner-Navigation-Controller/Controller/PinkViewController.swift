d8d384f33196374a39914e7bdceb73921d0a04d6
Ahtazaz Khan <mr.ahtazaz@gmail.com>
2020-04-02 12:08:46 +0500

    Initial Commit

---
+++ Beginner-Navigation-Controller/Controller/PinkViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PinkViewController.swift
+//  Beginner-Navigation-Controller
+//
+//  Created by Ahtazaz Khan on 4/2/20.
+//  Copyright © 2020 AhtazazKhan. All rights reserved.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = "Pink Screen"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Beginner-Navigation-Controller/Controller/TealViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TealViewController.swift
+//  Beginner-Navigation-Controller
+//
+//  Created by Ahtazaz Khan on 4/2/20.
+//  Copyright © 2020 AhtazazKhan. All rights reserved.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Teal Screen"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
